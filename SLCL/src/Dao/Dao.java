a4831fdfea3fb2f4402dcb3ef186417cddb7395c
tassinha <tanaka.tassia@gmail.com>
2013-04-13 13:05:04 -0300

    Configuração do Hibernate.

---
+++ SLCL/src/Dao/Dao.java
@@ -0,0 +1,55 @@
+package Dao;
+
+import java.util.List;
+import org.hibernate.Session;
+
+public class Dao<T> {
+
+    private Class classe;
+    private Session session;
+
+    public Dao(Class classe) {
+        this.session = HibernateUtil.getSession();
+        this.classe = classe;
+    }
+
+    public void save(T t) {
+       this.session = HibernateUtil.getSession();
+        this.session.save(t);
+        this.session.close();
+    }
+
+    public void delete(T t) {
+        this.session.beginTransaction();
+        this.session.delete(t);
+        this.session.close();
+    }
+
+    public void alter(T t) {
+        this.session.beginTransaction();
+        this.session.update(t);
+        this.session.close();
+    }
+
+    public T procura(Integer id) {
+        // TODO Auto-generated method stub
+        return (T) session.load(this.classe, id);
+    }
+
+    public List<T> findAll(T t) {
+        this.session.beginTransaction();
+        return this.session.createCriteria(this.classe).list();
+    }
+
+    public List<T> findAllLimit(int limite) {
+        List<T> list;
+        this.session.beginTransaction();
+        list = this.session.createCriteria(this.classe).setMaxResults(limite).list();
+        this.session.close();
+        return list;
+    }
+
+    public List<T> consultaSql(String Sql) {
+        return (List<T>) session.createSQLQuery(Sql).addEntity(classe).list();
+    }
+}
