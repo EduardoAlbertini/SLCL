37c41a1907a38bb6cc52098a09d87e20744558a6
tassinha <tanaka.tassia@gmail.com>
2013-04-24 19:41:28 -0300

    Deletando classe Autor e AutorDao e inserindo classe PedidoDeLivro.

--- SLCL2/src/java/br/com/entitys/Coordenador.java
+++ SLCL2/src/java/br/com/entitys/Coordenador.java
@@ -1,6 +1,7 @@
 package br.com.entitys;
 
 import javax.persistence.Entity;
+import javax.persistence.OneToOne;
 import javax.persistence.PrimaryKeyJoinColumn;
 import javax.persistence.Table;
 
@@ -8,8 +9,30 @@ import javax.persistence.Table;
 @Table(name = "Coordenador")
 @PrimaryKeyJoinColumn(name = "id")
 public class Coordenador extends Professor{
+    @OneToOne
+    private Curso curso;
+    
     public Coordenador() {
         super();
         super.setTipo(Tipo.COORDENADOR);
+    }
+    
+    public Coordenador(Curso curso) {
+        super();
+        super.setTipo(Tipo.COORDENADOR);
+        this.curso = curso;
+    }
+
+    public Curso getCurso() {
+        return curso;
+    }
+
+    public void setCurso(Curso curso) {
+        this.curso = curso;
+    }
+
+    @Override
+    public String toString() {
+        return "Coordenador: " + super.getNome() + " " + super.getSobrenome() + " do curso "  + curso.toString();
     }
 }
