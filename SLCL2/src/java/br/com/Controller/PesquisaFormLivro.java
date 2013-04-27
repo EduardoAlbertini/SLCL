089c91625a401fd0f4cacec045547dd3fd79690c
Eduardo Albertini <albertinieduardo@hotmail.com>
2013-04-27 16:20:46 -0300

    Servlet FormLivro OK

--- SLCL2/src/java/br/com/Controller/PesquisaFormLivro.java
+++ SLCL2/src/java/br/com/Controller/PesquisaFormLivro.java
@@ -5,9 +5,12 @@
 package br.com.Controller;
 
 import br.com.XML.XMLReader;
+import br.com.dao.DaoGenerics;
+import br.com.dao.entitys.DaoLivro;
 import br.com.entitys.Livro;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.servlet.RequestDispatcher;
@@ -27,21 +30,6 @@ import org.hibernate.Session;
 public class PesquisaFormLivro extends HttpServlet {
 
     /**
-     * Processes requests for both HTTP
-     * <code>GET</code> and
-     * <code>POST</code> methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-    }
-
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
      * Handles the HTTP
      * <code>GET</code> method.
      *
@@ -54,52 +42,23 @@ public class PesquisaFormLivro extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         try {
-            String isbn = request.getParameter("ISBN");
-            
-            Livro livro = null;
-
-            XMLReader leitor = new XMLReader();
-            if (isbn != null) {
-                livro = leitor.getLivro(isbn);
-                
-            }
+            String isbn = request.getParameter("ISBN").trim();
             System.out.println("ISBN: " + isbn);
-            
-            request.setAttribute("livro", livro);
-            
-            response.getWriter().write(livro.toString());
-            
-//            ServletContext contexto = this.getServletContext();
-//            contexto.setAttribute("livro", livro);
-            
-            System.out.println("ENTROU");
+            if (!isbn.equals("")) {
+                DaoGenerics dao = new DaoLivro();
+                List<Livro> livros = dao.listar("FROM Livro WHERE isbn = " + isbn);
+
+                Livro livro = livros.get(0);
+
+                if (livro == null) {
+                    XMLReader leitor = new XMLReader();
+                    livro = leitor.getLivro(isbn);
+                }
+
+                response.getWriter().write(livro.toString());
+            }
         } catch (Exception ex) {
             Logger.getLogger(PesquisaFormLivro.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-
-    /**
-     * Handles the HTTP
-     * <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        System.out.println("ENTROU NO SERVLET");
-    }
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
 }
