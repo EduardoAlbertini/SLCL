261ec393c281378e0a5dd279bcb41abca8c9e6b7
tassinha <tanaka.tassia@gmail.com>
2013-05-08 13:56:41 -0300

    Adição de Papel.

--- SLCL2/src/java/br/edu/utfpr/cm/slcl/dao/entitys/DaoPapel.java
+++ SLCL2/src/java/br/edu/utfpr/cm/slcl/dao/entitys/DaoPapel.java
@@ -1,7 +1,7 @@
 package br.edu.utfpr.cm.slcl.dao.entitys;
 
-import br.edu.utfpr.cm.slcl.dao.DaoGenerics;
 import br.edu.utfpr.cm.saa.entidades.Papel;
+import br.edu.utfpr.cm.slcl.dao.DaoGenerics;
 
 public class DaoPapel extends DaoGenerics<Papel> {
     public DaoPapel(){
--- SLCL2/src/java/br/edu/utfpr/cm/slcl/database/CriarBanco.java
+++ SLCL2/src/java/br/edu/utfpr/cm/slcl/database/CriarBanco.java
@@ -1,22 +1,9 @@
 package br.edu.utfpr.cm.slcl.database;
 
-import br.edu.utfpr.cm.saa.entidades.Papel;
 import br.edu.utfpr.cm.slcl.dao.PopularDao;
-import br.edu.utfpr.cm.slcl.dao.entitys.DaoBibliotecario;
-import br.edu.utfpr.cm.slcl.entitys.Bibliotecario;
 
 public class CriarBanco {
     public static void main(String[] args) {
         PopularDao.criarBanco();
-        
-        Bibliotecario bibliotecario = new Bibliotecario();
-        bibliotecario.setLogin("bibliotecario");
-        bibliotecario.setNome("Bibliotecario");
-        bibliotecario.setSenha("bibliotecario123");
-        bibliotecario.setEmail("bib@bib.com.br");
-        bibliotecario.setPapel(new Papel(3));
-        
-        DaoBibliotecario daoB = new DaoBibliotecario();
-        daoB.persistir(bibliotecario);
     }
 }
