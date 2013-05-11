84383494ae7db57de9aa4e6fe1114759b5304b8c
Eduardo Albertini <albertinieduardo@hotmail.com>
2013-05-11 11:36:48 -0300

    ComboBox Coordenador OK

---
+++ SLCL2/src/java/br/edu/utfpr/cm/slcl/Controller/ComboBoxCoordenador.java
@@ -0,0 +1,59 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.utfpr.cm.slcl.Controller;
+
+import br.edu.utfpr.cm.slcl.dao.entitys.DaoCoordenador;
+import br.edu.utfpr.cm.slcl.entitys.Coordenador;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Miray
+ */
+@WebServlet(name = "ComboBoxCoordenador", urlPatterns = {"/ComboBoxCoordenador"})
+public class ComboBoxCoordenador extends HttpServlet {
+
+    /**
+     * Handles the HTTP
+     * <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        List<Coordenador> coordenadores = new DaoCoordenador().listar();
+        
+        request.getSession().setAttribute("coordenadores", coordenadores);
+        
+        response.sendRedirect("crudCurso.jsp");
+    }
+
+    /**
+     * Handles the HTTP
+     * <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        doGet(request, response);
+    }
+
+}
